9f742bd26ce1eb0d716880c65aa03e97163bcdaf
zegemenim <zegemenim@gmail.com>
2026-01-20 23:49:49 +0300

    Intro To Adaboost Classifier

---
+++ 16-AdaboostClassifier.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load dataset
+data = pd.read_csv('Datasets/diabetes.csv')
+
