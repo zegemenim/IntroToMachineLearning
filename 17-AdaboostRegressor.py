513648c6db53463ee5ead953d9d9a768aa9a32fe
zegemenim <zegemenim@gmail.com>
2026-01-23 22:45:00 +0300

    Intro to adaboost regressor

---
+++ 17-AdaboostRegressor.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Datasets/17-cardekho.csv")
+print(df.head())
