2c77db1c44d84797e2540f1de71d9e052eb7ed60
zegemenim <zegemenim@gmail.com>
2026-01-27 19:25:32 +0300

    Add Gradient Boosting Regressor implementation with dataset loading and initial data exploration

---
+++ 18-GradientBoostingRegressor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import RandomizedSearchCV
+
+# Load dataset
+data = pd.read_csv("18-concrete_data.csv")
+print(data.head())
+print(data.info())
+print(data.describe())
+print(data.isnull().sum())
